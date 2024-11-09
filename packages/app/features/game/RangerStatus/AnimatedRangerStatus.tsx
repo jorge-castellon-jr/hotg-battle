e73244d1ea33667de4e2680c701c64193fcddf8e
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2024-11-08 16:12:54 -0800

    Rearanges screen and updates ranger status

--- packages/app/features/game/RangerStatus/AnimatedRangerStatus.tsx
+++ packages/app/features/game/RangerStatus/AnimatedRangerStatus.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
-import { Button, Sheet, Text, XStack, YStack } from 'tamagui'
-import Animated, { useAnimatedStyle, withSpring, useSharedValue } from 'react-native-reanimated'
+import React from 'react'
+import { Button, Sheet, Text, XStack, YStack, styled } from 'tamagui'
 import { RangerStatusCard } from './RangerStatusCard'
 import useGameStore, { GameState, Turn } from '../gameStateStore'
 import { DrawButton } from './DrawButton'
 import { rangerColors } from '../utils/colors'
 import { RangerDecks } from '../GameTypes'
 
-const AnimatedYStack = Animated.createAnimatedComponent(YStack)
+// Styled components with animation configurations
+const AnimatedContainer = styled(YStack, {
+  position: 'absolute',
+  bottom: 0,
+  width: '100%',
+  padding: '$2',
+  backgroundColor: '$background',
+  borderTopWidth: 1,
+  borderTopColor: '$borderColor',
+})
 
-const SPRING_CONFIG = {
-  damping: 15,
-  stiffness: 150,
-  mass: 0.5,
-}
+const DrawButtonsContainer = styled(XStack, {
+  gap: '$2',
+  variants: {
+    isVisible: {
+      true: {
+        height: 44,
+        marginBottom:"$2"
+      },
+      false: {
+        height: 0,
+      },
+    },
+  } as const,
+})
 
 interface RangerStatusProps {
   rangers: RangerDecks
@@ -29,72 +46,37 @@ export const AnimatedRangerStatus: React.FC<RangerStatusProps> = ({ rangers }) =
     showRangerInfo,
     setSelectedPosition,
   } = useGameStore()
-  const translateY = useSharedValue(0)
-  const drawTranslateY = useSharedValue(0)
-
-  React.useEffect(() => {
-    translateY.value = withSpring(
-      playedCard ? 75 : 0, // Move 200 units down when not visible
-      SPRING_CONFIG
-    )
-  }, [playedCard])
 
   const canDraw = (gameState === GameState.draw || !setupCompleted) && turn === Turn.player
-  React.useEffect(() => {
-    drawTranslateY.value = withSpring(
-      canDraw ? -69 : 69, // Move 200 units down when not visible
-      SPRING_CONFIG
-    )
-  }, [gameState, turn, setupCompleted])
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateY: translateY.value }],
-    }
-  }, [translateY])
 
   return (
-    <>
-      <AnimatedYStack
-        position="absolute"
-        bottom={0}
-        width="100%"
-        padding="$2"
-        gap="$2"
-        backgroundColor="$background"
-        borderTopWidth={1}
-        borderTopColor="$borderColor"
-        style={animatedStyle}
-      >
-        {canDraw && (
-          <XStack gap="$2">
-            {Object.entries(rangers).map(([position, ranger]) => (
-              <YStack key={`${position}-draw`} flex={1} flexBasis="100%">
-                <DrawButton
-                  key={position}
-                  onPress={() => {
-                    setSelectedPosition(position as 'left' | 'middle' | 'right')
-                    drawCard()
-                  }}
-                  color={rangerColors[ranger.color]}
-                />
-              </YStack>
-            ))}
-          </XStack>
-        )}
-
-        <XStack gap="$2">
-          {Object.entries(rangers).map(([position, ranger]) => (
-            <RangerStatusCard
+    <AnimatedContainer animation="quick">
+      <DrawButtonsContainer animation="quick" isVisible={canDraw}>
+        {Object.entries(rangers).map(([position, ranger]) => (
+          <YStack key={`${position}-draw`} flex={1} flexBasis="100%">
+            <DrawButton
               key={position}
-              ranger={ranger}
               onPress={() => {
-                showRangerInfo(position as 'left' | 'middle' | 'right')
+                setSelectedPosition(position as 'left' | 'middle' | 'right')
+                drawCard()
               }}
+              color={rangerColors[ranger.color]}
             />
-          ))}
-        </XStack>
-      </AnimatedYStack>
-    </>
+          </YStack>
+        ))}
+      </DrawButtonsContainer>
+
+      <XStack gap="$2">
+        {Object.entries(rangers).map(([position, ranger]) => (
+          <RangerStatusCard
+            key={position}
+            ranger={ranger}
+            onPress={() => {
+              showRangerInfo(position as 'left' | 'middle' | 'right')
+            }}
+          />
+        ))}
+      </XStack>
+    </AnimatedContainer>
   )
 }
--- packages/app/features/game/screen.tsx
+++ packages/app/features/game/screen.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { YStack } from 'tamagui'
 import useGameStore, { Turn } from './gameStateStore'
 import TopBar from './components/TopBar'
 import EnemyRow from './components/EnemyRow'
@@ -12,26 +11,20 @@ const GameScreen: React.FC = () => {
   const { rangerDecks, hand, enemies, turn } = useGameStore()
 
   return (
-    <YStack flex={1} backgroundColor="$background" position="relative" overflow="hidden">
-      {/* Top Section */}
+    <>
       <TopBar
         currentTurn={turn === Turn.player ? "Ranger's Turn" : "Enemy's Turn"}
         turn={0}
         onHistoryRequest={() => { }}
       />
 
-      <BattleSequence />
-
-      {/* Enemy Section */}
       <EnemyRow enemies={enemies} />
 
-      {/* Hand Section */}
       <RangerHand hand={hand} />
-
-      {/* Ranger Status - Fixed at bottom */}
       <RangerStatusOverview rangers={rangerDecks} />
+      <BattleSequence />
       <UiOverlay />
-    </YStack>
+    </>
   )
 }
 
