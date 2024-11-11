7aea18d4a5ba44e7a6924657be1d0caef11f84fc
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2024-11-11 15:32:50 -0800

    disables pwa on dev

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -29,7 +29,7 @@ const plugins = [
   }),
   withPWA({
     dest: 'public',
-    // disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === 'development',
     register: true,
     skipWaiting: true,
   }),
