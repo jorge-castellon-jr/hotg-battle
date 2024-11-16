f30e01845d3db1a83ba26048cc3d57bb563bb962
jorge-castellon-jr <31225261+jorge-castellon-jr@users.noreply.github.com>
2024-11-15 23:29:09 -0800

    Update cardDatabase.ts

--- packages/app/features/game/DB/cardDatabase.ts
+++ packages/app/features/game/DB/cardDatabase.ts
@@ -1,10 +1,12 @@
 import MightyMorphinTeam from './MightyMorphin'
 import ZeoTeam from './Zeo'
+import rangers from './rangers'
 import { RangerCard } from '../Card/CardTypes'
 
 const cardDatabase: RangerCard[] = [
   ...MightyMorphinTeam,
   ...ZeoTeam,
+  ...rangers,
   //
 ]
 
